af5911ba8724fe31f1b73527fe8b9d26344bb842
MrNightOmsk <mrnightomsk@gmail.com>
2025-03-03 03:40:25 +0600

    fix: исправлен формат URL для подключения к базе данных

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -44,13 +44,14 @@ class Settings(BaseSettings):
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
         if isinstance(v, str):
             return v
-        return PostgresDsn.build(
+        url = PostgresDsn.build(
             scheme="postgresql",
             username=values.get("POSTGRES_USER"),
             password=values.get("POSTGRES_PASSWORD"),
             host=values.get("POSTGRES_SERVER"),
             path=f"/{values.get('POSTGRES_DB') or ''}",
         )
+        return str(url)
 
     ELASTICSEARCH_HOST: str = "elasticsearch"
     ELASTICSEARCH_PORT: int = 9200
