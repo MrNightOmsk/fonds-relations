f3f7f9e285ce9d33034ef834c451d8a4123773a8
MrNightOmsk <mrnightomsk@gmail.com>
2025-03-02 20:20:19 +0600

    Полная очистка проекта. Оставлены только README.md, docs/ и output.json

--- components/ui/loading-overlay.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingSpinner } from "./loading-spinner"
-
-export function LoadingOverlay() {
-  return (
-    <div className="absolute inset-0 flex items-center justify-center bg-background/80">
-      <LoadingSpinner className="h-8 w-8" />
-    </div>
-  )
-} 
--- frontend/jest.setup.js
+++
@@ -1,6 +0,0 @@
-require('@testing-library/jest-dom')
-const { server } = require('./src/mocks/server')
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close()) 
